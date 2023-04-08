36c371936acab48d128b5130ca89fdfc44d0ad9f
Brajananda Mahata <75053676+Brajo280603@users.noreply.github.com>
2023-04-08 15:45:20 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ for(let i = 0;i<=4096;i++){//16777215 for full
   hexCol = String(hexCol).padStart(3, '0');
   console.log("#"+hexCol);
 
-  let colorCode = "#"+hexCol;
+  let colorCode = hexCol.split("").reverse().join("");
+  colorCode = "#"+colorCode; 
 
   let html = `<div class="card" style="background-color: ${colorCode};">
                 <p class="card-text">${colorCode}</p>
