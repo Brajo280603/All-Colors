f60ca1eb1df5e536d00a0f03c5cae6ca4c05ed93
Brajananda Mahata <brajanandamahato280603@gmail.com>
2023-04-08 13:12:14 +0530

    not all colors

---
+++ main.js
@@ -0,0 +1,25 @@
+// let colVar = "#000000";
+let num = 0;
+let canvasDiv = document.getElementById("fullCanvas");
+
+for(let i = 0;i<=4096;i++){//16777215 for full
+  let hexCol = num.toString(16);
+  // hexCol = String(hexCol).padStart(6, '0'); for full
+  hexCol = String(hexCol).padStart(3, '0');
+  console.log("#"+hexCol);
+
+  let colorCode = "#"+hexCol;
+
+  let html = `<div class="card" style="background-color: ${colorCode};">
+                <p class="card-text">${colorCode}</p>
+              </div>`
+
+  canvasDiv.insertAdjacentHTML("beforeend", html);
+
+
+
+  num++;
+}
+
+
+
